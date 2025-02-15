ca10ee78c219e4bf9508a1daceff37985851804b
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-02-15 03:40:53 +0100

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,5 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  { "rebelot/kanagawa.nvim", priority = 1000, config = true, opts = ... },
+  { "rose-pine/neovim", name = "rose-pine" },
+}
